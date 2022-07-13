f3dfc5668e6eb0ec224ea04b98530a2fc254bbab
bhokaresneha <bhokaresneha20@gmail.com>
2022-07-13 08:13:08 +0530

    EmployeePayroll Application Using SpringBoot

---
+++ src/main/java/com/example/employeepayroll_springboot/EmployeePayrollSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeepayroll_springboot/EmployeePayrollSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
