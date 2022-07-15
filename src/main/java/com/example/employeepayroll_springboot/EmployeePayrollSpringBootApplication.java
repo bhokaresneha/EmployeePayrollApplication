1e6473cbc3ee84ae88f97bf3533431d70a96a86a
bhokaresneha <bhokaresneha20@gmail.com>
2022-07-15 09:16:08 +0530

    Added : Database setting as environment variable.

--- src/main/java/com/example/employeepayroll_springboot/EmployeePayrollSpringBootApplication.java
+++ src/main/java/com/example/employeepayroll_springboot/EmployeePayrollSpringBootApplication.java
@@ -4,14 +4,21 @@ import com.example.employeepayroll_springboot.model.EmployeePayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication
 public class EmployeePayrollSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollSpringBootApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollSpringBootApplication.class, args);
         log.info("Using Lombok Library for Logging");
+     //uc-8
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+     //uc-9
+        log.info("Employee Payroll DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
 
 
     }
