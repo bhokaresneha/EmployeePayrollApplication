1655e9e18b1261797abdb02dcaeabf7f83565f8a
bhokaresneha <bhokaresneha20@gmail.com>
2022-07-15 15:44:23 +0530

    Added : Ability to throw User Friendly Errors in case Employee Id is not found in Employee Payroll App

---
+++ src/main/java/com/example/employeepayroll_springboot/exception/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.employeepayroll_springboot.exception;
+
+import com.example.employeepayroll_springboot.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMsg = errorList.stream()
+                .map(objErr -> objErr.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO respDTO = new ResponseDTO("Exception while processing REST request", errMsg);
+        return new ResponseEntity<>(respDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(EmployeePayrollException.class)
+    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
+        ResponseDTO respDTO = new ResponseDTO("Exception while processing REST request",
+                exception.getMessage());
+        return new ResponseEntity<>(respDTO, HttpStatus.BAD_REQUEST);
+    }
+}
